e782d09b12d39b26ce040b9b442b539a3e11be8b
V4RSH1TH-R3DDY <b.varshithreddy123@gmail.com>
2026-04-14 19:33:18 +0530

    target files added

---
+++ target/debug/deps/rust_os-63af9a094f4eb753.d
@@ -0,0 +1,5 @@
+/home/varshith/rust-os/target/debug/deps/rust_os-63af9a094f4eb753.d: src/main.rs
+
+/home/varshith/rust-os/target/debug/deps/rust_os-63af9a094f4eb753: src/main.rs
+
+src/main.rs:
---
+++ target/x86_64-unknown-linux-gnu/debug/deps/rust_os-7364049ca8aa384a.d
@@ -0,0 +1,5 @@
+/home/varshith/rust-os/target/x86_64-unknown-linux-gnu/debug/deps/rust_os-7364049ca8aa384a.d: src/main.rs
+
+/home/varshith/rust-os/target/x86_64-unknown-linux-gnu/debug/deps/rust_os-7364049ca8aa384a: src/main.rs
+
+src/main.rs:
